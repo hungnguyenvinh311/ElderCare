22e23d79becc5786ace2aefe2a2e682cfba996e7
hungnguyenvinh311 <hungbum205@gmail.com>
2025-11-21 22:26:52 +0700

    Delete src directory

--- src/main/java/com/hungdev/firstproject/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hungdev.firstproject.repository;
-
-import com.hungdev.firstproject.entity.RoleEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<RoleEntity,Long> {
-    RoleEntity findOneByCode(String code);
-}
--- src/main/java/com/hungdev/firstproject/service/MedicalProfileService.java
+++
@@ -1,8 +0,0 @@
-package com.hungdev.firstproject.service;
-
-import com.hungdev.firstproject.model.MedicalProfileDTO;
-
-public interface MedicalProfileService {
-    MedicalProfileDTO getMedicalProfile(Integer useId);
-    MedicalProfileDTO addAndUpdateMedicalProfile(MedicalProfileDTO dto);
-}
